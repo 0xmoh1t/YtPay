b95e62cb6da24723e5cd6584c96a84ab1e14ca70
Anand Singh <anandteam9777@gmail.com>
2025-09-27 03:31:51 +0530

    Stop tracking .env

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useActiveAccount } from "thirdweb/react"; 
 import ConnectWallet from "./pages/connectwallet";
-
+import SearchChannel from './pages/SearchChannel';
 
 export default function App() {
   const account = useActiveAccount();
@@ -10,9 +10,14 @@ export default function App() {
     <div>
       <header style={{ textAlign: "center", marginTop: "20px" }}>
         <h1>Welcome</h1>
-        {!account && <ConnectWallet />}
+      <ConnectWallet />
       </header>
 
+      {account && (
+        <div style={{ marginTop: "30px" }}>
+          <SearchChannel />
+        </div>
+      )}
     </div>
   );
 }
